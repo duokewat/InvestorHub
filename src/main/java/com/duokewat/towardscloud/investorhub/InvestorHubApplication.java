d991dbec16c94f4f4f87d56f9978589e959086a9
Boobathi Ayyasamy <boobathi.ayyasamy@gmail.com>
2022-06-18 02:42:59 +0530

    Initial Commit

---
+++ src/main/java/com/duokewat/towardscloud/investorhub/InvestorHubApplication.java
@@ -0,0 +1,13 @@
+package com.duokewat.towardscloud.investorhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestorHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestorHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duokewat/towardscloud/investorhub/InvestorHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duokewat.towardscloud.investorhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestorHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
